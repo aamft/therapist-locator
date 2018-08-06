6596e786bf66e987fca8e9fa19542196e53ee81d
Justin Prevatte <jnprevatte@gmail.com>
2018-08-06 09:50:56 -0400

    add cookie handling to z-custom.js and add terms-of-service.js

--- terms-of-service.js
+++ terms-of-service.js
@@ -1,51 +1,32 @@
-//get specified cookie value
-function getCookie(cname)
-{
-var name = cname + "=";
-var ca = document.cookie.split(';');
-for(var i=0; i<ca.length; i++)
-  {
-  var c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-  }
-return "";
-}
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
 jQuery().ready(function(){
 	//get survey cookie value
-	var thisHereCookie=getCookie("tosc");
+	var thisHereCookie=getCookie("tltos");
 	
 	//if no survey taken yet
 	if (thisHereCookie!="1"){
 
-//display the overlay and blur results list on newer browsers
-jQuery('#masterContentArea').css('filter','blur(3px)');
-jQuery('[id*="tosol"]').show().css('display','flex');
+		//display the overlay and blur results list on newer browsers
+		jQuery('#masterContentArea').css('filter','blur(3px)');
+		jQuery('[id*="tosol"]').show().css('display','flex');
 
-//tos accepted button clicked
-jQuery('.tosmod button').click(function(){
+		//tos accepted button clicked
+		jQuery('.tosmod button').click(function(){
 
-	//create cookie
-	//document.cookie="tosc=1; expires=Thu, 23 Jul 2017 12:00:00 GMT; domain=therapistlocator.net; path=/";		
-	setCookie('tosc', 1, 14);
+			//create cookie
+			//document.cookie="tosc=1; expires=Thu, 23 Jul 2017 12:00:00 GMT; domain=therapistlocator.net; path=/";		
+			setCookie('tosc', 1, 14);
 
-	//hide the overlay and unblur the results list
-	jQuery('#masterContentArea').css('filter','blur(0px)');
-	jQuery('[id*="tosol"]').hide();
+			//hide the overlay and unblur the results list
+			jQuery('#masterContentArea').css('filter','blur(0px)');
+			jQuery('[id*="tosol"]').hide();
 
-	//create ga event for acceptance
-	//ga('send', 'event', 'terms of service', 'accepted');
+			//create ga event for acceptance
+			//ga('send', 'event', 'terms of service', 'accepted');
 
-	//don't let the click do anything else
-	return false;
-});
+			//don't let the click do anything else
+			return false;
+		});
 
-}
+	}
 
 });
--- z-custom.js
+++ z-custom.js
@@ -37,3 +37,22 @@ jQuery('#usSearch').submit(function() {
         jQuery(this).attr('action','/tl/therapist-finder-name.aspx');
     }
 });
+//GET COOKIE VALUE
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++)
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+return "";
+}
+//SET COOKIE
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
